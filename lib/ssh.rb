40ee6772c516acbdae1f9b775dffc331d7a7f98e
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-11-09 16:52:25 +0300

    add exec to ssh

--- lib/ssh.rb
+++ lib/ssh.rb
@@ -1,8 +1,16 @@
+require "net/ssh"
+
 class Ssh
-  def initialize
+  def initialize host, ip, user, password
     @host = host
     @ip = ip
     @user = user
     @password = password 
+  end
+
+  def exec command
+    Net::SSH:.start(@ip, @user, @passwrod) do |ssh|
+      ssh.exec!(command) 
+    end
   end
 end
