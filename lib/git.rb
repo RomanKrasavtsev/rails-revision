e32ac87e81c8d7737250bc2f1ac1a8a95e1aa051
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-11-18 14:11:41 +0300

    update git

--- lib/git.rb
+++ lib/git.rb
@@ -3,8 +3,7 @@ class Git
     @path = path
   end
 
-  def self.get_revision
+  def get_revision
     `SHA1=$(git rev-parse --short HEAD 2> /dev/null); if [ $SHA1 ]; then echo $SHA1; else echo 'unknown'; fi`.chomp
   end
 end
-
