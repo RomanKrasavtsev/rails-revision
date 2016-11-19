e7d30ee2523ee871573d14326047621a419f2ebb
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-11-19 18:23:08 +0300

    update ssh

--- lib/ssh.rb
+++ lib/ssh.rb
@@ -1,16 +1,17 @@
 require "net/ssh"
 
 class Ssh
-  def initialize host, ip, user, password
+  def initialize(host, ip, proxy, user, password)
     @host = host
     @ip = ip
+    @proxy = proxy
     @user = user
-    @password = password 
+    @password = password
   end
 
-  def exec command
-    Net::SSH.start(@ip, @user, @passwrod) do |ssh|
-      ssh.exec!(command) 
+  def exec(command)
+    Net::SSH.start(@proxy ? @proxy : @ip, @user, password: @password) do |ssh|
+      @proxy ? ssh.exec!("ssh user@#{@ip} '#{command}'") : ssh.exec!(command)
     end
   end
 end
