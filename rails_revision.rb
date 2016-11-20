e8dcea2175d06a35659b1d60320a711a79c9c79c
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-11-20 17:53:52 +0300

    update rails_revision

--- rails_revision.rb
+++ rails_revision.rb
@@ -1,11 +1,18 @@
 require "sinatra"
 require_relative "lib/ssh"
+require_relative "lib/capistrano"
+require_relative "lib/git"
+require_relative  "settings"
 
 get "/" do
-  erb :index, :locals => {
-    servers: [
-      "server1",
-      "server2"
-    ]
-  }
+  @revisions = []
+  SERVERS.each do |server|
+    host = server[0].to_s
+    proxy = server[1][:proxy] ? server[1][:proxy] : nil
+    ssh = Ssh.new(host, server[1][:ip], proxy, USER, PASSWORD)
+    utility = (server[1][:type] == "capistrano") ? Capistrano.new(PATH) : Git.new(PATH)
+    @revisions << {server: host, revision: ssh.exec(utility.get_revision)}
+  end
+
+  erb :index
 end
