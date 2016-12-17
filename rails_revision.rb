8453a09d5a56b6fc2836d2960de5f1899ef397dd
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-12-17 07:03:47 +0300

    add start date and time

--- rails_revision.rb
+++ rails_revision.rb
@@ -13,7 +13,12 @@ get "/" do
     proxy = server[1][:proxy] ? server[1][:proxy] : nil
     ssh = Ssh.new(host, server[1][:ip], proxy, USER, PASSWORD)
     utility = (server[1][:type] == "capistrano") ? Capistrano.new(PATH) : Git.new(PATH)
-    @revisions << {server: host, revision: ssh.exec(utility.get_revision)}
+    @revisions << {
+      server: host,
+      revision: ssh.exec(utility.get_revision),
+      start_date_time: ssh.exec(
+        "ls -ld /proc/$(cat /var/www/shared/pids/unicorn.pid) | awk '{print $6\" \"$7}'")
+      }
   end
 
   erb :index
